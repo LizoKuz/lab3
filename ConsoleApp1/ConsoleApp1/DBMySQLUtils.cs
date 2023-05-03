0b573283cf0cedabe01288fa09eda032e6d5ee1a
LizoKuz <105271140+LizoKuz@users.noreply.github.com>
2023-05-03 23:59:58 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/DBMySQLUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp1
+{
+     class DBMySQLUtils
+    {
+        public static MySqlConnection
+            GetDBConnection(string host,int port,string database,string username, string password)
+        {
+            String connString = "Server=" + host + ";Database=" + database
+                + ";port=" + port + ";User ID=" + username + ";password=" + password;
+            MySqlConnection conn = new MySqlConnection(connString);
+            return conn;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.Common;
+using ConsoleApp1;
+using MySql.Data.MySqlClient;
+
+ static void QueryEmployee(MySqlConnection conn)
+{
+    string Code, Batch_name, Quality,size,date_registration;
+    string sql = "Select Code, Batch_name, Quality,size,date_registration from batch_products";
+    MySqlCommand cmd = new MySqlCommand();
+    cmd.Connection = conn;
+    cmd.CommandText = sql;
+
+    using (DbDataReader reader = cmd.ExecuteReader())
+    {
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                Code = reader["Code"].ToString();
+                Batch_name = reader["batch_name"].ToString();
+                Quality = reader["Quality"].ToString();
+                size = reader["Size"].ToString();
+                date_registration = reader["Date_registration"].ToString();
+                Console.WriteLine("---------------------------------------------------------------");
+                Console.WriteLine("Код:" + Code + " Назва:" + Batch_name + 
+                    " Гатунок:" + Quality + " Розмір:"+ size + " Дата регістрації:"+ date_registration);
+                Console.WriteLine("---------------------------------------------------------------");
+            }
+        }
+    }
+}
+Console.WriteLine("Getting Connection...");
+MySqlConnection conn = DBUtils.GetDBConnection();
+try
+{
+    Console.WriteLine("Openning Connection ...");
+    conn.Open();
+    Console.WriteLine("Connection successful!");
+    QueryEmployee(conn);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Error" + e.Message);
+    Console.WriteLine(e.StackTrace);
+}
+finally
+{
+    conn.Close();
+    conn.Dispose();
+}
+Console.Read();
